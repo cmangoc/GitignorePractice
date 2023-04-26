677d60fd4130b35f217f0d69891fe29bf92508dd
Chris <cmangotti3@gmail.com>
2023-04-26 10:08:56 -0400

    added the rest

---
+++ GitignorePractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitignorePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
